ece4f906d4dfe8da0d5971e8710ca6b4c1f7fb94
Angier24 <djangier507pty@gmail.com>
2024-06-18 23:23:55 -0500

    Codigos python agregados

---
+++ py Angier/Problema 13.py
@@ -0,0 +1,6 @@
+# Problema 12
+celsius= float(input("ingresar grados celcius:"))
+f=((celsius*1.8)+32)
+k=(celsius+273.15)
+print(f)
+print(k)
---
+++ py Angier/Problema 6.py
@@ -0,0 +1,6 @@
+# Problema 6
+largo = float(input("Digite largo:"))
+ancho = float(input("Digite ancho:"))
+altura = float(input("Digite altura:"))
+v = largo * ancho * altura
+print("El volumen del prisma es:",v)
